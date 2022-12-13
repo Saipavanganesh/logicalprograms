c624c2ccffd20b1d59e15f813f101e7153810d40
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-14 05:23:46 +0530

    Fibonacci Series

--- src/com/logicalprograms/LogicalPrograms.java
+++ src/com/logicalprograms/LogicalPrograms.java
@@ -1,7 +1,23 @@
 package com.logicalprograms;
 
+import java.util.Scanner;
+
 public class LogicalPrograms {
     public static void main(String[] args) {
-        System.out.println("Logical Programs");
+        System.out.println("Fibonacci Series");
+        System.out.println("****************");
+        System.out.println("");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+        int sum=0,a=0,b=1;
+        System.out.print(a+" "+b);
+        for(int i=2;i<num;i++){
+            sum=a+b;
+            System.out.print(" "+sum);
+            a=b;
+            b=sum;
+        }
     }
 }
