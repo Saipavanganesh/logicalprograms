18815afe5d5c47828231d387849d09b05b835c82
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-14 05:37:20 +0530

    Prime Number

--- src/com/logicalprograms/LogicalPrograms.java
+++ src/com/logicalprograms/LogicalPrograms.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class LogicalPrograms {
     public static void main(String[] args) {
-        System.out.println("Perfect Number");
-        System.out.println("**************");
+        System.out.println("Prime Number");
+        System.out.println("************");
         System.out.println(" ");
 
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
         int num=sc.nextInt();
-        int sum=0;
-        /* Identifying the Factors of the number */
-        for(int i=1;i<num;i++){
+        int count=0;
+
+        for(int i=2;i<num;i++){
             if(num%i==0){
-                System.out.println(i);
-                sum=sum+i;
+                count++;
             }
         }
-        if(sum==num){
-            System.out.println("Perfect number");
+        if(num==0 || num==1){
+            System.out.println(num+" is not prime nor composite");
+        }
+        else if(count==0){
+            System.out.println("Prime number");
         }
         else{
-            System.out.println("Not a perfect number");
+            System.out.println("Not a prime number");
         }
     }
 }
